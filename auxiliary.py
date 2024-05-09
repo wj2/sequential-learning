8f4741d3999ffb189a14c3390925080c9926efc0
wjj <wjeffreyjohnston@gmail.com>
2024-05-09 11:57:14 -0400

    Added default value

--- auxiliary.py
+++ auxiliary.py
@@ -15,7 +15,7 @@ CONFIG_PATH = "sequential_learning/config.conf"
 cf = u.ConfigParserColor()
 cf.read(CONFIG_PATH)
 
-BASEFOLDER = cf.get("DEFAULT", "BASEFOLDER")
+BASEFOLDER = cf.get("DEFAULT", "BASEFOLDER", "../data/sequential_learning/")
 
 
 ft1 = (
