Fixed bug in computation

sequential_learning/analysis.py
@@ -1061,7 +1061,7 @@ def quantify_task_error_pattern(
     null_map = null_map.flatten()
     vec = choice_map - null_map
     vec_norm = np.nansum(vec ** 2)
-    mag = np.nansum(proj_map * vec) / vec_norm
+    mag = np.nansum((proj_map - null_map) * vec) / vec_norm
     res = pd.DataFrame(
         np.stack((proj_map.flatten(), choice_map.flatten()), axis=1)
     ).corr().to_numpy()[0, 1]

sequential_learning/visualization.py
@@ -464,7 +464,7 @@ def visualize_map_matching(result_dict, res_key, shape_seq=None, axs=None, fwid=
         if len(res[res_key]) > 2:
             print("mag")
             mag = res[res_key][2]
-            print(mag)
+            print(np.mean(mag))
             gpl.plot_trace_werr(sessions, mag, ax=axs[i], conf95=True)
         axs[i].set_title(shape)
         axs[i].set_ylabel("correlation")